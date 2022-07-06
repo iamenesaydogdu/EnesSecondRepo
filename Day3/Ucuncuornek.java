664f765a9144a091761faa445904a1d39f01dff3
iamenesaydogdu <ens_aydogdu@hotmail.com>
2022-07-05 17:33:32 -0700

    hey this is my msg

--- Day3/Ucuncuornek.java
+++
@@ -1,8 +0,0 @@
-package Day3;
-
-public class Ucuncuornek {
-
-    public static void main(String[] args) {
-        System.out.println("ucuncu ornek yapiyoruz");
-    }
-}
---
+++ UcuncuornekMAC/UcuncuORNEK.java
@@ -0,0 +1,9 @@
+package UcuncuornekMAC;
+
+public class UcuncuORNEK {
+
+
+    public static void main(String[] args) {
+        System.out.println("UCUNCU ORNEK YAPIYORUZ");
+    }
+}
