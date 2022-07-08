96c1f78c4c9937ad5df56d4511223733392acb8d
iamenesaydogdu <108760131+iamenesaydogdu@users.noreply.github.com>
2022-07-07 17:56:56 -0700

    Update JoePractice.java

--- JoeBranch/JoePractice.java
+++ JoeBranch/JoePractice.java
@@ -4,5 +4,7 @@ public class JoePractice {
     public static void main(String[] args) {
 
         System.out.println("HEYYY this is Joe Branch , I just checkout it ");
+        
+        System.out.println("Birseyler degistirdik JoeBranch icinde ki Joe Practice de  ");
     }
 }
